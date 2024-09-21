9ee1cd2b270234d2d92709fd268bdc7d30652073
Navjot Singh <navjotsingh16082003@gmail.com>
2024-09-21 13:38:52 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,81 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Auth from './pages/auth'
-import Chat from './pages/chat'
-import Profile from './pages/profile'
-import { useAppStore } from './store'
-import apiClient from './lib/api-client'
-import { GET_USER_INFO } from './utils/constants'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Auth from './pages/auth';
+import Chat from './pages/chat';
+import Profile from './pages/profile';
+import { useAppStore } from './store';
+import apiClient from './lib/api-client';
+import { GET_USER_INFO } from './utils/constants';
 
 const PrivateRoute = ({ children }) => {
-  const {userInfo} = useAppStore()
-  const isAuthenticated = !!userInfo
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
 
-  return isAuthenticated ? children : <Navigate to="/auth"/>
-}
+  return isAuthenticated ? children : <Navigate to="/auth" />;
+};
 
 const AuthRoute = ({ children }) => {
-  const {userInfo} = useAppStore()
-  const isAuthenticated = !!userInfo
-  return isAuthenticated ? <Navigate to="/chat" /> : children
-}
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to="/chat" /> : children;
+};
 
 const App = () => {
-
-  const {userInfo, setUserInfo} = useAppStore()
-  const [loading, setLoading] = useState(true)
-
+  const { userInfo, setUserInfo } = useAppStore();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUserData = async () => {
-      try{
-        const res = await apiClient.get(GET_USER_INFO, {withCredentials: true,})
-        if(res.status === 200 && res.data.id){
+      try {
+        const res = await apiClient.get(GET_USER_INFO, { withCredentials: true });
+        if (res.status === 200 && res.data.id) {
           setUserInfo(res.data);
-        }else{
+        } else {
           setUserInfo(undefined);
-        } 
-      }catch(error){
-        setUserInfo(undefined)
+        }
+      } catch (error) {
+        setUserInfo(undefined);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
 
-    if(!userInfo){
-      getUserData()
-    }else{
-      setLoading(false)
+    if (!userInfo) {
+      getUserData();
+    } else {
+      setLoading(false);
     }
+  }, [userInfo, setUserInfo]);
 
-  }, [userInfo,setUserInfo])
-  
-  if(loading){
-    return <div className=""> Loading </div>
+  if (loading) {
+    return <div className=""> Loading </div>;
   }
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={
-            <AuthRoute> 
-              <Auth/> 
-            </AuthRoute>} />
-          <Route path="/chat" element={
-            <PrivateRoute> 
-              <Chat/> 
-              </PrivateRoute>} />
-          <Route path="/profile" element={
-            <PrivateRoute> 
-              <Profile/> 
-              </PrivateRoute>
-            } />
-          <Route path="*" element={<Navigate to={<Auth/>}/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={
+          <AuthRoute> 
+            <Auth/> 
+          </AuthRoute>} 
+        />
+        <Route path="/chat" element={
+          <PrivateRoute> 
+            <Chat/> 
+          </PrivateRoute>} 
+        />
+        <Route path="/profile" element={
+          <PrivateRoute> 
+            <Profile/> 
+          </PrivateRoute>
+        } />
+        <Route path="/" element={<Navigate to="/auth" />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
